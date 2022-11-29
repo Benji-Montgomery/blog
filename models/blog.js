c91de71ecb54c276559f34a63cd7a8991c7ea812
Benji-Montgomery <benjimon678@yahoo.com>
2022-11-29 04:24:02 -0800

    1129backup

--- models/blog.js
+++ models/blog.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose')
-
-const url = process.env.MONGODB_URI
 console.log('twist')
-console.log('connecting to', url)
+
 // mongoose.connect(url)
 //   .then(() => {
 //     console.log('connected to MongoDB')
