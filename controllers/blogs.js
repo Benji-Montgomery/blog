4025ee3e2b288bf0c2eec1f9b71e611b4b12b204
Benji-Montgomery <benjimon678@yahoo.com>
2022-12-03 16:36:13 -0800

    4.20

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,16 +3,17 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7)
-  }
-  return null
-}
+// const getTokenFrom = request => {
+//   const authorization = request.get('authorization')
+//   if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+//     return authorization.substring(7)
+//   }
+//   return null
+// }
 
 
 blogsRouter.get('/', (request, response) => {
+  console.log(request.token)
   Blog.find({}).then(blogs => {
     response.json(blogs)
   })
@@ -32,8 +33,8 @@ blogsRouter.get('/:id', (request, response, next) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
-
-  const token = getTokenFrom(request)
+  console.log('micey')
+  const token = (request.token)
   const decodedToken = jwt.verify(token, process.env.SECRET)
   if (!token || !decodedToken.id) {
     return response.status(401).json({ error: 'token missing or invalid' })
